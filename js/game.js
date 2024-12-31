2a870ac6b284f20491786a2c5cabe72c4737ab01
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-12-31 21:15:11 +0000

    add TODOs and some initial boilerplate

---
+++ js/game.js
@@ -0,0 +1,15 @@
+function create_vertex_elm(x, y) {
+  let vertex_elm = document.createElement("span");
+  vertex_elm.classList.add("np_vertex");
+  vertex_elm.style["top"] = "";
+  // TODO
+}
+
+function add_edge_element(x1, y1, x2, y2) {
+  // TODO
+}
+
+// TODO actually read the alphine docs lol
+// TODO load graph from json
+// TODO data structure for ur graph
+// TODO fn that calculates if u have a valid route
